467d1236e19ce1809f263fadcf4389e4e2bd18cf
Colton Medlin <coltonmedlin@gmail.com>
2021-04-01 09:10:51 -0700

    mongo documents properly updating

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  id: Number,
+  id: {type: Number, unique: true},
   name: String,
   description: String,
   url: String,
@@ -15,18 +15,33 @@ let repoSchema = mongoose.Schema({
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
+//Repo.createIndex({id: 1}, {unique: true});
 
 let save = (data) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
-  Repo.insertMany(data, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(result);
-    }
+
+  // Repo.insertMany(data, (err, result) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log(result);
+  //     return result;
+  //   }
+  // });
+
+  data.forEach((item) => {
+   Repo.updateOne({id: item.id}, item, {upsert: true}, (err, result) => {
+     if (err) {
+       console.log('error', err);
+     } else {
+       console.log('success', result);
+     }
+   });
   });
+
+
 }
 
 module.exports.save = save;
