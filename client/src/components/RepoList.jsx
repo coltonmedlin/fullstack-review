1349bd461f108cfccf26ee93c6523f2c898a9cb4
Colton Medlin <coltonmedlin@gmail.com>
2021-04-01 15:50:04 -0700

    data loading on page load and updating on submit

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const RepoList = (props) => (
   <div>
-    <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
+    <h4> TOP REPOS </h4>
+    <div>
+    {props.repos.map((repo) =>
+    <div>
+      <a href={repo.url}>{repo.name}</a>
+      <p>{repo.description}</p>
+      <p>{repo.stargazersCount}</p>
+      </div>
+    )}
+    </div>
   </div>
 )
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,22 +10,35 @@ class App extends React.Component {
     this.state = {
       repos: []
     }
+    this.search = this.search.bind(this);
+    this.render = this.render.bind(this);
+  }
 
+  componentDidMount () {
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:1128/repos',
+      contentType: "application/json",
+      success: (response) => {
+        this.setState({repos: response});
+        console.log('state', this.state.repos);
+      }
+    });
   }
 
   search (term) {
-    console.log(`${term} was searched`);
+    //console.log(`${term} was searched`);
 
     $.ajax({
       type: 'POST',
       url: 'http://localhost:1128/repos',
       contentType: "application/json",
       data: JSON.stringify({handle: term}),
-      success: (res) => {
-        console.log('response:', res);
+      success: (response) => {
+        this.setState({repos: response});
+        console.log('state', this.state.repos);
       }
     });
-
   }
 
   render () {
