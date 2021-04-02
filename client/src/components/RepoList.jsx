dc33edca07ec472640a0f758ec7c5e9b445eae0a
Colton Medlin <coltonmedlin@gmail.com>
2021-04-01 17:56:17 -0700

    complete adding links - app working properly

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
+let num = 0;
+
 const RepoList = (props) => (
   <div>
     <h4> TOP REPOS </h4>
     <div>
-    {props.repos.map((repo) =>
+    {props.repos.map((repo) => {
+      num++;
+    return(
     <div>
-      <a href={repo.url}>{repo.name}</a>
+      #{num} <a href={repo.url}>{repo.name}</a>
       <p>{repo.description}</p>
-      <p>{repo.stargazersCount}</p>
+      <p>	&#9734; 	&#9734; 	&#9734; 	&#9734; {repo.stargazersCount}</p>
       </div>
-    )}
+    );
+    })}
     </div>
   </div>
 )
